290aa82f3bf89c0fac0b81321b542eb13a1b86a4
MamaDutchy <87340915+MamaDutchy@users.noreply.github.com>
2022-06-11 19:52:05 -0400

    Updated to Unity Version 2021.3.4f

--- Assets/Scripts/LoadLevelOnCollision.cs
+++ Assets/Scripts/LoadLevelOnCollision.cs
@@ -58,6 +58,7 @@ public class LoadLevelOnCollision : MonoBehaviour
                 Time.timeScale = 1;
                 Time.fixedDeltaTime = Time.timeScale * 0.02f;
                 SceneManager.LoadScene(strSceneName);
+                cam.GetComponent<PostProcess>().enabled = true;
                 isAtEndOfLevel = false;
             }
         }
