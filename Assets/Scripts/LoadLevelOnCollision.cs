45580dbff7a3ed7095afda7eb43edbdb8124e088
MamaDutchy <87340915+MamaDutchy@users.noreply.github.com>
2022-06-11 19:21:13 -0400

    made text box appear big and center at end of level

--- Assets/Scripts/LoadLevelOnCollision.cs
+++ Assets/Scripts/LoadLevelOnCollision.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadLevelOnCollision : MonoBehaviour
 {
@@ -12,9 +13,19 @@ public class LoadLevelOnCollision : MonoBehaviour
     string strSceneName;
 
     public GameObject cam;
+    public Image scoreBox;
+    private float t = 0;
+
+    private Vector2 originalPosition;
     public  float endOfLevelTime;
     private float timeThusFar = 0;
     private bool isAtEndOfLevel;
+
+    private void Start()
+    {
+        originalPosition = scoreBox.rectTransform.anchoredPosition;
+    }
+
 
     // Restart Scene on Collision
     private void OnCollisionEnter(Collision collision)
@@ -26,16 +37,17 @@ public class LoadLevelOnCollision : MonoBehaviour
         }
     }
 
-   void FixedUpdate()
+   void Update()
     {
         EndLevel();
+        
     }
 
 
     private void EndLevel() {
         if (isAtEndOfLevel){
-            timeThusFar += Time.unscaledDeltaTime;
-            //Move Score to center
+            DisplayScore();
+            timeThusFar += Time.unscaledDeltaTime * 0.001f;
             if (timeThusFar <= endOfLevelTime)
             {
                 Time.timeScale -= 0.3f * Time.unscaledDeltaTime;
@@ -50,4 +62,20 @@ public class LoadLevelOnCollision : MonoBehaviour
             }
         }
     }
+
+    private void DisplayScore()
+    {
+        t += Time.unscaledDeltaTime*2; 
+
+        t = Mathf.Clamp(t, 0, 1);
+        float newX = Mathf.SmoothStep(originalPosition.x,0, t);
+        float newY = Mathf.SmoothStep(originalPosition.y,0, t);
+        float newScale = Mathf.SmoothStep(1, 2, t);
+        //scoreBox.rectTransform.position.Set(newX, newY,0);
+        scoreBox.rectTransform.anchoredPosition = new Vector2(newX, newY);
+        scoreBox.rectTransform.localScale = new Vector3(newScale, newScale, 1);
+        
+
+    }
+
 }
