52a146f82e4ebd0fac6dce846cd015a1b9f5e1da
Dan Prince <danpprince@gmail.com>
2022-09-02 20:02:34 -0400

    Add SplineMesh package

---
+++ Assets/Scripts/FrameRateLimiter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRateLimiter : MonoBehaviour
+{
+    void Awake () {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = 100;
+    }
+}
