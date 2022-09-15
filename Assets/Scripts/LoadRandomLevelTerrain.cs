a956cf0fb1c0fc543c13a3ac5dafc9082ead8382
MamaDutchy <87340915+MamaDutchy@users.noreply.github.com>
2022-09-15 15:18:41 -0400

    "camera now sees level" need to mute and make buttons

--- Assets/Scripts/LoadRandomLevelTerrain.cs
+++ Assets/Scripts/LoadRandomLevelTerrain.cs
@@ -6,13 +6,16 @@ public class LoadRandomLevelTerrain : MonoBehaviour
 {
     private string LevelPrefabs = "TerrainForMenu";
     private Object[] levels;
+    public Camera cam;
+    public GameObject place;
 
     // Start is called before the first frame update
     void Start()
     {
         levels = Resources.LoadAll(LevelPrefabs) as Object[];
         int randomLevel = Random.Range(0, levels.Length);
-        GameObject.Instantiate(levels[randomLevel]);
+        GameObject curLevel = (GameObject)GameObject.Instantiate(levels[randomLevel],place.transform);
+        cam.GetComponent<LookAtLevel>().level = curLevel;
     }
 
     // Update is called once per frame
