f3b15a2068f43973cafc6b1e35925c710a49a954
Mama_Dutchy <datpoland@gmail.com>
2022-10-13 20:37:16 -0400

    SCALE WITH SCREEN SIZE

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(Vector3.back * Time.deltaTime * 100f);
-        this.transform.Rotate(Vector3.left * Time.deltaTime * 50f);
+        this.transform.Rotate(transform.forward);
     }
 }
