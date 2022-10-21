31d71b393713c094aef952e3e0645edccfc8293e
Colin Farrell <colinfarrell@protonmail.com>
2022-10-21 15:47:14 -0400

    added start button controller support for pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -20,7 +20,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().buildIndex != 0)
+        if (Input.GetButtonDown("Pause") && SceneManager.GetActiveScene().buildIndex != 0)
         {
             if (isPaused)
             {
